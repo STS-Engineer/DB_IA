7fc46df3871cc463fd00c152617cfcbeefb57774
STS-Engineer <administration.sts@avocarbon.com>
2025-08-28 11:26:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI, HTTPException, File, Form, UploadFile , Request , Response
-import httpx
+from fastapi import FastAPI, HTTPException, File, Form, UploadFile
 from pydantic import BaseModel, EmailStr             
 from models import (                                 
     ActionPlan,
